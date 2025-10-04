a047a140eb3aca1bf6a86ca73d70f380b56a5b6e
samarthkolur <samarthdkolur1@gmail.com>
2025-10-04 19:20:21 +0530

    added basic ui for the frontend

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ import passport from "passport";
 import mongoose from "mongoose";
 import session from "express-session";
 import MongoStore from "connect-mongo";
+import pkg from "mongodb";
 
+const { MongoClient: mongoClient } = pkg;
 const app=express();
 dotenv.config();
 app.use(cors(
@@ -14,7 +16,22 @@ app.use(cors(
         credentials: true,
     }
 ));
+async function connectDB(){
+    const client = new mongoClient(process.env.MONGO_URI);
+    try{
+        const db= client.db("mytheatre_db");
+        const user=db.collection("Users");
+        console.log("Connected to MongoDB");
+        return user;
+    } finally {
+    await client.close();
+  }
+}
 
+app.get("/Users", async(req,res)=>{
+    const users=await connectDB();
+    res.json(users);
+});
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
 });
